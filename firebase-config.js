9a71ef8eec06d01bb7cc33d339f0faf845519220
Amanda Walshjamin <amanda.jamin@gmail.com>
2025-05-28 14:33:53 -0400

    rename firebaseconfig.js to have an -

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+export const firebaseConfig = {
     apiKey: "AIzaSyA4vFPt7AllttEVTccPqx2mUkq3djDAX_M",
     authDomain: "hoby-ny-east-service-hours.firebaseapp.com",
     projectId: "hoby-ny-east-service-hours",
@@ -7,5 +7,3 @@ const firebaseConfig = {
     appId: "1:570357288484:web:ffd7fc0cd0a880db46d2b1",
     measurementId: "G-J5BP1KY64B"
 };
-
-export firebaseConfig;
