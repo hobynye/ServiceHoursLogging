3151094c2d9767b3d5257403d5558ea64b6959fd
Amanda Walshjamin <amanda.jamin@gmail.com>
2025-05-28 14:23:15 -0400

    when reusing an org display values from db

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4vFPt7AllttEVTccPqx2mUkq3djDAX_M",
+    authDomain: "hoby-ny-east-service-hours.firebaseapp.com",
+    projectId: "hoby-ny-east-service-hours",
+    storageBucket: "hoby-ny-east-service-hours.firebasestorage.app",
+    messagingSenderId: "570357288484",
+    appId: "1:570357288484:web:ffd7fc0cd0a880db46d2b1",
+    measurementId: "G-J5BP1KY64B"
+};
